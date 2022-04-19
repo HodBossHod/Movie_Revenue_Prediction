d91809d6d113bb968213cdd36c6723db2f9e6e18
Hady Ahmed Abd El-slame <61879050+HadyAhmed00@users.noreply.github.com>
2022-04-19 03:23:36 +0200

    merging the tables

--- main.py
+++ main.py
@@ -8,43 +8,89 @@ from sklearn.model_selection import train_test_split
 from sklearn.preprocessing import PolynomialFeatures
 from sklearn.preprocessing import StandardScaler
 import matplotlib.pyplot as plt
+from googlesearch import search
+import re
 
-# directors = pd.read_csv("movie-director.csv")
-# voice_actors = pd.read_csv("grouped_Actors_and_char.csv")
-# movie_revenue = pd.read_csv("movies-revenue.csv")
-# master = pd.DataFrame(
-#     columns=['movie_title', 'release_date', 'genre', 'MPAA_rating', 'revenue', 'director', 'voice-actor', 'movie'])
-
-# master = pd.merge(directors, movie_revenue, how='outer')
-# # master = pd.marge(master,movie_revenue)
-# # master.shape
-# master.to_csv('master.csv', index=False)
-# print(master.head())
-# master = pd.read_csv('master.csv')
-#
-# master1 = pd.merge(master, voice_actors, how='outer')
-#
-# master1.to_csv('final.csv', index=False)
-#
-# final = pd.read_csv('final.csv')
-# print(final.movie_title.nunique())
-#
-# # master.insert(-2, 'director', director)
-# # print(master.head())
-# grouped_char = voice_actors.groupby('movie_title')['character'].apply(list)
-# grouped_Actors = voice_actors.groupby('movie_title')['voice-actor'].apply(list)
-# grouped_Actors.to_csv('grouped_Actors.csv')
-# grouped_char.to_csv('grouped_char.csv')
-# grouped_char = pd.read_csv ('grouped_char.csv')
-# grouped_Actors = pd.read_csv('grouped_Actors.csv')
-# grouped_Actors_and_char = pd.merge(grouped_Actors,grouped_char,how='outer')
-# grouped_Actors_and_char.to_csv('grouped_Actors_and_char.csv')
-# revenue_actors = pd.merge(voice_actors, movie_revenue, how='outer')
-# revenue_actors.to_csv('revenue_actors.csv')
-# revenue_actors = pd.read_csv('revenue_actors.csv')
-# full_data_movie = pd.merge(revenue_actors, directors, how='outer')
-# full_data_movie.to_csv('full_movies_data.csv')
-movies_data = pd.read_csv('full_movies_data.csv')
-data_withoutNulls = movies_data.fillna(value=0)
+revenue_df = pd.read_csv('movies-revenue.csv')
+actor_df = pd.read_csv('movie-voice-actors.csv')
 
 
+# print(actor_df.isna().sum())
+
+def get_director(movie_name):
+    query = movie_name + ' director'  # the movie name +'director'
+    link_list = []
+    for j in search(query, tld="co.in", num=1, stop=1, pause=2):
+        link_list.append(j)
+
+    if len(link_list) != 0:
+        link_comp = link_list[0].split('/')
+        director = link_comp[-1]  # the director name
+        director = str(director)
+        if len(director) <= 43:
+            while '-' in director:
+                director = director.replace('-', ' ')
+            while '_' in director:
+                director = director.replace('_', ' ')
+            if str(movie_name).lower() in director.lower():
+                return ''
+            director = re.sub("\(.*\)", '', director)
+            if 'film director'.lower() in director.lower():
+                director = director.replace('film director', '')
+            if re.compile(r"\d+").search(director):
+                return ''
+
+            print(movie_name + ' : ' + str(director))
+            return director
+        else:
+            return ''
+    else:
+        return ''
+
+
+def fill_new_director():
+    # getting the movie directors
+    directors = {'movie_title': [], 'director': []}
+    move_title = list(revenue_df['movie_title'])
+    for move in move_title:
+        directors['movie_title'].append(move)
+        directors['director'].append(get_director(move))
+    df_directors = pd.DataFrame.from_dict(directors, orient='index').T
+    print(df_directors.head())
+    df_directors.to_csv('new_directors.csv')
+    return df_directors
+
+
+# director_df = fill_new_director()
+print(revenue_df.shape)
+director_df = pd.read_csv('new_directors.csv')
+print(director_df.shape)
+rev_dir_df = pd.merge(revenue_df, director_df, how='outer')
+print(rev_dir_df.shape)
+# print(rev_dir_df.isna().sum())
+# print(list(rev_dir_df['director'].value_counts()))
+
+is_animation_d = {'movie_title': [], 'is_animation': []}
+movies_list = list(rev_dir_df['movie_title'])
+actor_movies_list = list(actor_df['movie_title'])
+
+for movie in movies_list:
+    if movie in actor_movies_list:
+        is_animation_d['movie_title'].append(str(movie))
+        is_animation_d['is_animation'].append(1)
+    else:
+        is_animation_d['movie_title'].append(str(movie))
+        is_animation_d['is_animation'].append(0)
+
+is_animation_df = pd.DataFrame.from_dict(is_animation_d, orient='index').T
+print(is_animation_df.shape)
+movies_df = pd.merge(rev_dir_df, is_animation_df, how='outer')
+print(movies_df.shape)
+# movies_df.to_csv('final.csv')
+# print(movies_df.isna().sum())
+movies_df = movies_df.fillna(0)
+# removing the dollar sign from movies_df columns
+movies_df = movies_df.apply(lambda x: x.str.strip('$') if x.dtype == "object" else x)
+# removing the comma from the numeric columns
+movies_df = movies_df.apply(lambda x: x.replace(',', "", regex=True) if x.dtype == "object" else x)
+movies_df.to_csv('final.csv')
